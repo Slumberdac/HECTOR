706b8feecca48b4ca72b3c081dd1185c6a8efab8
LoicBurri-Rochon <2322244@cmontmorency.qc.ca>
2024-05-07 09:13:03 -0400

    navbar

--- frontend/src/components/Navigation/Nav.jsx
+++ frontend/src/components/Navigation/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./Nav.css";
-import SvgBurgerMenu from "../../assets/BurgerMenu";
+import Pfp from "../User/Pfp";
 function Nav() {
 	const [isNavOpen, setIsNavOpen] = useState(false);
 	const toggleNav = () => {
@@ -10,6 +10,7 @@ function Nav() {
 	return (
 		<>
 			<div className="nav-container">
+				<Pfp color="#999999" eye={15} mouth={8} className="pfp" />
 				<div className="users-button">Users</div>
 				<div className="rocks-button">Companions</div>
 			</div>
