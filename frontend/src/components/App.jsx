68b07e2c51ced890b164d51697bf56b4c6033c8d
LoicBurri-Rochon <2322244@cmontmorency.qc.ca>
2024-05-07 09:12:34 -0400

    Update App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,10 @@
 import Welcome from "./Welcome/Welcome";
-import Pfp from "./User/Pfp";
+
 import Nav from "./Navigation/Nav";
 import "./App.css";
 const App = () => {
 	return (
 		<>
-			<Pfp color="#2FC" eye={9} mouth={8} />
 			<Nav />
 			<Welcome />
 		</>
