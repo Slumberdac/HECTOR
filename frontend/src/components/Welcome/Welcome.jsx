d0d4203a839c19c8d3493a230030ca6b189bb896
BytesOf87 <99377178+BytesOf87@users.noreply.github.com>
2024-05-09 12:34:51 -0400

    idk man I backgrounded

--- frontend/src/components/Welcome/Welcome.jsx
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -23,7 +23,6 @@ function Welcome() {
 	return (
 		<div className="welcome">
 			<div className="welcome-background">
-				<img id="rocks-bg" src={Background} alt="background" />
 			</div>
 			<div className="welcome-logo">
 				<h1>Welcome to</h1>
