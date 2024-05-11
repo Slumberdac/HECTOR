2ecdf4e63dccd19ada581e9f57674890b7118971
LoicBurri-Rochon <2322244@cmontmorency.qc.ca>
2024-05-11 12:14:07 -0400

    search bar

--- frontend/src/components/User/UsersRegistry.jsx
+++ frontend/src/components/User/UsersRegistry.jsx
@@ -13,12 +13,21 @@ export default function UsersRegistry() {
 				setUserList(data.users);
 			});
 	}, []);
+	const [searchTerm, setSearchTerm] = useState("");
+	const filteredList = userList?.filter((item) =>
+		item["username"].toLowerCase().includes(searchTerm.toLowerCase())
+	);
 	return (
 		<div>
 			<h1>Users Registry</h1>
-
+			<input
+				type="text"
+				placeholder="Rechercher"
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
 			<ul className="users-registry">
-				{userList.map((user) => (
+				{filteredList?.map((user) => (
 					<UserCard key={user._id} user={user} />
 				))}
 			</ul>
