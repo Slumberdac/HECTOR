0b049040cffd7e147a7e6f2116e82c290c2b24f9
BytesOf87 <99377178+BytesOf87@users.noreply.github.com>
2024-05-14 09:05:44 -0400

    Roches libres avant roches adoptées

--- frontend/src/components/Rock/RocksRegistry.jsx
+++ frontend/src/components/Rock/RocksRegistry.jsx
@@ -32,7 +32,14 @@ export default function RocksRegistry() {
 			/>
 			<ul className="rocks-registry">
 				{filteredList?.map((rock) => (
-					<RockCard key={rock._id} rock={rock} />
+					!rock.owner && (
+						<RockCard key={rock._id} rock={rock} />
+					)
+				))}
+				{filteredList?.map((rock) => (
+					rock.owner && (
+						<RockCard key={rock._id} rock={rock} />
+					)
 				))}
 				<AddRockCard />
 			</ul>
